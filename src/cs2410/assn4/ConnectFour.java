658156c1f4e9dcecbf6758508bb411202d51d21f
sephpace <sephpace@gmail.com>
2019-02-21 09:17:16 -0700

    first commit.  setup ConnectFour.java

---
+++ src/cs2410/assn4/ConnectFour.java
@@ -0,0 +1,30 @@
+package cs2410.assn4;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * The popular Connect Four game.
+ *
+ * @author   Joseph (Seph) Pace
+ * @version  1.0
+ */
+public class ConnectFour extends Application {
+    /**
+     * Main method.
+     *
+     * @param args  Arguments from the command line: none expected.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Starts the JavaFX application.
+     *
+     * @param stage  The primary stage.
+     */
+    public void start(Stage stage) {
+
+    }
+}
